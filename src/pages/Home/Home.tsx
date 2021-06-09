3a15ce9f308c7a89a62903dfb0bff4b33d01ba0a
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-06-09 17:08:37 -0500

    fix problems with narvbar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,21 +8,22 @@ const HomePage: React.FunctionComponent<IPage & RouteComponentProps<any>> = prop
   const navBarStyles = () => {
     const header = document.getElementById("header");
     const navcontent = document.getElementById("nav-content");
-    const navaction = document.getElementById("navAction");
+    const navButton = document.getElementById("navButton");
     // const brandname = document.getElementById("brandname");
     const toToggle = document.querySelectorAll(".toggleColour");
     const slinqerLogo = document.getElementById("logo") as HTMLImageElement;
     const scrollpos = window.scrollY;
     /*Apply classes for slide in bar*/
 
-    if (header && navaction && slinqerLogo && navcontent) {
+    if (header && navButton && slinqerLogo && navcontent) {
       if (scrollpos > 10 ) {
         header.classList.add("bg-white");
-        header.classList.remove("gradient");
-        // navaction.classList.remove("bg-white");
-        // navaction.classList.add("gradient");
-        navaction.classList.remove("text-gray-800");
-        navaction.classList.add("text-white");
+        // header.classList.remove("gradient");
+        // Nav Button
+        navButton.classList.remove("bg-white");
+        navButton.classList.add("gradient");
+        navButton.classList.remove("text-gray-800");
+        navButton.classList.add("text-white");
         slinqerLogo.src = './assets/images/logo.svg'
         //Use to switch toggleColour colours
         for (var i = 0; i < toToggle.length; i++) {
@@ -36,11 +37,12 @@ const HomePage: React.FunctionComponent<IPage & RouteComponentProps<any>> = prop
       } else {
         slinqerLogo.src = './assets/images/logo_symbolic.svg'
         header.classList.remove("bg-white");
-        header.classList.add("gradient");
-        // navaction.classList.remove("gradient");
-        // navaction.classList.add("bg-white");
-        navaction.classList.remove("text-white");
-        navaction.classList.add("text-gray-800");
+        // header.classList.add("gradient");
+        // BUtton
+        navButton.classList.remove("gradient");
+        navButton.classList.add("bg-white");
+        navButton.classList.remove("text-white");
+        navButton.classList.add("text-gray-800");
         //Use to switch toggleColour colours
         for (let i = 0; i < toToggle.length; i++) {
           toToggle[i].classList.add("text-white");
@@ -62,11 +64,11 @@ const HomePage: React.FunctionComponent<IPage & RouteComponentProps<any>> = prop
     document.addEventListener('scroll', navBarStyles)
   }, [])
   return (
-    <div id="home" className="tracking-normal text-white">
+    <div id="home" className="tracking-normal ">
       {/* <!--Nav--> */}
-      <nav id="header" className="fixed w-full z-30 top-0 text-white gradient">
+      <nav id="header" className="fixed w-full z-30 top-0 text-white bg-white">
 
-        <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+        <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2" >
 
           <div className="pl-4 flex items-center">
             <a className=" text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="!">
@@ -85,7 +87,9 @@ const HomePage: React.FunctionComponent<IPage & RouteComponentProps<any>> = prop
             </button>
           </div>
 
-          <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
+          <div id="nav-content"
+            className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
+          >
             <ul className="list-reset lg:flex justify-end flex-1 items-center">
               <li className="mr-3">
                 <a className="toggleColour text-white font-bold inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="!">
@@ -93,7 +97,7 @@ const HomePage: React.FunctionComponent<IPage & RouteComponentProps<any>> = prop
                 </a>
               </li>
             </ul>
-            <button id="navAction" className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75">
+            <button id="navButton" className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75">
               Contacto
             </button>
           </div>
