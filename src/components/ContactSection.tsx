f8ea497e5340814f215c997b532cac622078c61c
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-06-19 19:59:23 -0500

    fix: remove console errors

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -47,11 +47,11 @@ const ContactSection = (props: any) => {
                 <div className="h-1 gradient w-64 opacity-25 my-2 py-0 rounded-t"></div>
               </div>
               <div className="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"
                   viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"
                     d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"
                     d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
                 <div className="ml-4 text-md tracking-wide  w-40">
@@ -60,9 +60,9 @@ const ContactSection = (props: any) => {
               </div>
 
               <div className="flex items-center mt-2 mb-6 text-gray-600 dark:text-gray-400">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"
                   viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
                 <a href="mailto:latam@slinqer.com"className="ml-4 text-md tracking-wide font-semibold w-40 hover:underline">
@@ -76,11 +76,11 @@ const ContactSection = (props: any) => {
                 <div className="h-1 gradient w-64 opacity-25 my-2 py-0 rounded-t"></div>
               </div>
               <div className="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"
                   viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"
                     d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
                 <div className="ml-4 text-md tracking-wide w-40">
                   Lithuania
@@ -88,9 +88,9 @@ const ContactSection = (props: any) => {
               </div>
 
               <div className="flex items-center mt-2 mb-6 text-gray-600 dark:text-gray-400">
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"
                   viewBox="0 0 24 24" className="w-8 h-8 text-gray-500">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
                 <a href="mailto:europe@slinqer.com" className="ml-4 text-md tracking-wide font-semibold w-40 hover:underline">
