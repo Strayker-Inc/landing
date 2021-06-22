c5baa105b4fbaec78c9e1875784826f6b722ba71
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-06-21 19:39:53 -0500

    feat: testing background image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,22 @@
+const styleSection: any  = {
+	'background-image': "url('./assets/images/background.png')",
+};
+
 const Hero = (props: any) =>
-  <section className="flex justify-center  font-inter">
-    <div className="mt-60 mb-28 text-center ">
-      <h1 className="text-5xl font-extrabold text-gray-800  md:text-6xl">
+  <section className="flex h-screen bg-fixed font-inter opacity-95" style={styleSection}>
+    <div className="m-auto text-center">
+      <h1 className="text-5xl font-extrabold text-white  md:text-6xl shadow opacity-90">
         <span>{props.t('hero.title')}</span>
         <br />
-        <span className="text-green">{props.t('hero.titleColored')}</span>
+        <span className="text-green shadow">{props.t('hero.titleColored')}</span>
       </h1>
-      <p className="mt-8 text-gray-500  sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl">
+      <p className="mt-8 text-white font-semibold  sm:text-lg sm:max-w-xl sm:mx-auto md:text-2xl">
         {props.t('hero.text')}
       </p>
       <div className="mt-5 sm:mt-8 flex justify-center">
         <div className="rounded-md ">
           <a href="#contactForm"
-            className="w-full flex items-center justify-center mx-auto  hover:underline gradient text-white font-inter font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75">
+            className="w-full bg-white flex items-center justify-center mx-auto  hover:underline text-gray-800 font-inter font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75">
             {props.t('hero.buttons.contact')}
           </a>
         </div>
