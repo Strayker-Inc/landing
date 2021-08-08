e398e80288217d16a63580dd084812ff09dfc1f5
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-08-08 12:30:54 -0500

    feat: make spanish default logo

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import common_en from "./translations/en/common.json";
 
 i18next.init({
     interpolation: { escapeValue: false },  // React already does escaping
-    lng: 'en',
+    lng: 'es',
       resources: {
         en: {
           common: common_en
