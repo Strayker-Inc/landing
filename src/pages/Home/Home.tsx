e9093c72150298621f1ed0edb02bf699a1bc44c5
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-08-08 12:31:45 -0500

    feat: simplify landing view

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { RouteComponentProps, withRouter  } from "react-router-dom";
-// Interfaces
 import IPage from "../../interfaces/page";
 import './Home.css'
-// Components
-import ServicesCards from '../../components/ServicesCards'
-import AboutUs from '../../components/AboutUsSection';
-import Footer from '../../components/Footer';
-import ContactSection from '../../components/ContactSection';
-import Partners from "../../components/Partners";
+// import ServicesCards from '../../components/ServicesCards'
+// import AboutUs from '../../components/AboutUsSection';
+// import Footer from '../../components/Footer';
+// import ContactSection from '../../components/ContactSection';
+// import Partners from "../../components/Partners";
 import Hero from "../../components/Hero";
 import Navbar from "../../components/Navbar";
 import {useTranslation} from "react-i18next";
@@ -18,26 +16,19 @@ const HomePage: React.FunctionComponent<IPage & RouteComponentProps<any>> = prop
 
   return (
     <div className="tracking-normal font-inter">
-      {/* <!--Nav--> */}
       <Navbar t={t} />
 
-      {/* <!-- HERO --> */}
       <Hero t={t} />
 
-      {/* <!-- ABOUT US--> */}
-      <AboutUs t={t}/>
+      {/* <AboutUs t={t}/>
 
-      {/* <!-- OUR PARTNERS --> */}
       <Partners t={t} />
 
-      {/* <!-- OUR SERVICES --> */}
       <ServicesCards t={t}/>
 
-      {/* <!-- CONTACT US --> */}
       <ContactSection t={t}/>
 
-      {/* <!-- Begin Footer Section--> */}
-      <Footer t={t}/>
+      <Footer t={t}/> */}
 
     </div>
   );
