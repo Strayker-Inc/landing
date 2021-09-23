365118aad0ed6293833891454955825a44ed9337
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-09-23 09:07:18 -0500

    New partner and fade effect

--- src/components/AboutUsSection.tsx
+++ src/components/AboutUsSection.tsx
@@ -1,3 +1,5 @@
+const Fade = require('react-reveal/Fade');
+
 const sectionStyle: React.CSSProperties = {
   // scrollSnapAlign: "start",
   height: '100vh',
@@ -5,28 +7,31 @@ const sectionStyle: React.CSSProperties = {
 
 const AboutUsSection = (props: any) =>
   <section className="flex justify-center bg-gray-100 " style={sectionStyle}>
-    <div className="container items-center flex flex-wrap">
-      <div className="w-6/12 md:w-3/12 mx-auto">
-        <img alt="cepillos de bambu" className="rounded-lg shadow-lg" src="./assets/images/brushes.webp"/>
-      </div>
+    <Fade bottom cascade>
+      <div className="container items-center flex flex-wrap">
+        <div className="w-6/12 md:w-3/12 mx-auto">
+          <img alt="cepillos de bambu" className="rounded-lg shadow-lg" src="./assets/images/brushes.webp"/>
+        </div>
 
-      <div className="w-full md:w-5/12 mx-auto px-4">
-        <div className="lg:pr-10">
-          <div className="hidden md:visible p-3 text-center inline-flex justify-center w-12 h-12 mb-2 md:w-16 md:h-16 md:mb-6 shadow-lg rounded-full bg-white">
-            <p className="text-2xl md:text-3xl">🌎</p>
-          </div>
-          <h2 className="text-4xl md:text-5xl mb-4 font-black text-gray-800">{props.t('aboutUs.title')}</h2>
-          <p className="text-2xl text-gray-600 mb-8 md:text-3xl ">
-            {props.t('aboutUs.text')}
-          </p>
-          <div className="flex justify-center">
-            <img src="./assets/images/ecopoop.webp" alt="Logo aliado Ecopoop" className="h-16 mr-4 md:h-20 md:mr-8" />
-            <img src="./assets/images/lifepack.webp" alt="Logo aliado Lifepack" className="h-16 mr-4 md:h-24 md:mr-8" />
+        <div className="w-full md:w-5/12 mx-auto px-4">
+          <div className="lg:pr-10">
+            <div className="hidden md:visible p-3 text-center inline-flex justify-center w-12 h-12 mb-2 md:w-16 md:h-16 md:mb-6 shadow-lg rounded-full bg-white">
+              <p className="text-2xl md:text-3xl">🌎</p>
+            </div>
+            <h2 className="text-4xl md:text-5xl mb-4 font-black text-gray-800">{props.t('aboutUs.title')}</h2>
+            <p className="text-2xl text-gray-600 mb-8 md:text-3xl ">
+              {props.t('aboutUs.text')}
+            </p>
+            <div className="flex justify-center">
+              <img src="./assets/images/ecopoop.webp" alt="Logo aliado Ecopoop" className="h-16 mr-4 md:h-20 md:mr-8" />
+              <img src="./assets/images/lifepack.webp" alt="Logo aliado Lifepack" className="h-16 mr-4 md:h-24 md:mr-8" />
+              <img src="./assets/images/kaoba.webp" alt="Logo aliado Kaoba" className="h-16 mr-4 md:h-24 md:mr-8" />
+            </div>
           </div>
         </div>
-      </div>
 
-    </div>
+      </div>
+    </Fade>
   </section>
 
 export default AboutUsSection;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+const Fade = require('react-reveal/Fade');
+
+
 /* Page scroll effect https://www.youtube.com/watch?v=htw4iKMYzEc */
 const sectionStyle: React.CSSProperties = {
   // scrollSnapAlign: 'start',
@@ -11,21 +14,24 @@ const Hero = (props: any) => {
       <video className="absolute w-full h-screen object-cover" autoPlay loop muted>
         <source src="https://res.cloudinary.com/slinqer/video/upload/v1631900417/video_background_eshz8b.mp4" type="video/mp4" />
       </video>
-      <div className="text-center z-10">
-        <h1 className="text-4xl text-white mb-4 font-extrabold leading-none tracking-tight lg:text-6xl ">
-          <span>{props.t('hero.title')}</span>
-          &nbsp;
-          <span className="text-green">{props.t('hero.titleColored')}</span>
-        </h1>
-        <p className="text-2xl mb-8 text-white sm:max-w-lg sm:mx-auto ">
-          {props.t('hero.text')}
-        </p>
-        <a href="#contactForm">
-          <button className="w-5/6  md:w-1/2 p-4 text-xl bg-green text-white font-bold rounded-lg transform transition duration-500 hover:scale-150">
-            {props.t('hero.buttons.join')}
-          </button>
-        </a>
-      </div>
+      <Fade bottom cascade>
+        <div className="text-center z-10">
+          <h1 className="text-4xl text-white mb-4 font-extrabold leading-none tracking-tight lg:text-6xl ">
+            <span>{props.t('hero.title')}</span>
+            &nbsp;
+            <span className="text-green">{props.t('hero.titleColored')}</span>
+          </h1>
+          <p className="text-2xl mb-8 text-white sm:max-w-lg sm:mx-auto ">
+            {props.t('hero.text')}
+          </p>
+          <a href="#contactForm">
+            <button className="w-5/6  md:w-1/2 p-4 text-xl bg-green text-white font-bold rounded-lg transform transition duration-500 hover:scale-150">
+              {props.t('hero.buttons.join')}
+            </button>
+          </a>
+        </div>
+
+      </Fade>
       <img src="./assets/images/waves-inverted-gray.svg" className="absolute inset-x-0 bottom-0 w-full" alt="" />
     </section>
   );
