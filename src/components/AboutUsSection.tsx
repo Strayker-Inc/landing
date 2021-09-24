2319400883167559864fcf6bce7b8a14aaad7456
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-09-24 12:58:06 -0500

    new image and texts optimized

--- src/components/AboutUsSection.tsx
+++ src/components/AboutUsSection.tsx
@@ -1,21 +1,16 @@
 const Fade = require('react-reveal/Fade');
 
-const sectionStyle: React.CSSProperties = {
-  // scrollSnapAlign: "start",
-  height: '100vh',
-}
-
 const AboutUsSection = (props: any) =>
-  <section className="flex justify-center bg-gray-100 " style={sectionStyle}>
+  <section className="flex justify-center h-screen bg-gray-100" >
     <Fade bottom cascade>
-      <div className="container items-center flex flex-wrap">
-        <div className="w-6/12 md:w-3/12 mx-auto">
-          <img alt="cepillos de bambu" className="rounded-lg shadow-lg" src="./assets/images/brushes.webp"/>
+      <div className="w-11/12 md:w-9/12 items-center flex flex-wrap">
+        <div className="w-11/12 md:w-6/12 mx-auto">
+          <img alt="Mujer sosteniendo una planta" className="rounded-2xl shadow-lg" src="./assets/images/save_world.webp"/>
         </div>
 
-        <div className="w-full md:w-5/12 mx-auto px-4">
+        <div className="w-11/12 md:w-5/12 mx-auto">
           <div className="lg:pr-10">
-            <div className="hidden md:visible p-3 text-center inline-flex justify-center w-12 h-12 mb-2 md:w-16 md:h-16 md:mb-6 shadow-lg rounded-full bg-white">
+            <div className="p-3 text-center inline-flex justify-center w-12 h-12 mb-2 md:w-16 md:h-16 md:mb-6 shadow-lg rounded-full bg-white">
               <p className="text-2xl md:text-3xl">🌎</p>
             </div>
             <h2 className="text-4xl md:text-5xl mb-4 font-black text-gray-800">{props.t('aboutUs.title')}</h2>
