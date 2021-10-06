843764e53218417a5249f967e61798be3fa21a5e
Anderson Laverde Gracia <anderson.laverde@zohomail.com>
2021-10-06 15:51:17 -0500

    new partner

--- src/components/AboutUsSection.tsx
+++ src/components/AboutUsSection.tsx
@@ -4,11 +4,11 @@ const AboutUsSection = (props: any) =>
   <section className="flex justify-center h-screen bg-gray-100" >
     <Fade bottom cascade>
       <div className="w-11/12 md:w-9/12 items-center flex flex-wrap">
-        <div className="w-11/12 md:w-6/12 mx-auto">
+        <div className="w-11/12 md:w-6/12 mx-auto pr-4">
           <img alt="Mujer sosteniendo una planta" className="rounded-2xl shadow-lg" src="./assets/images/save_world.webp"/>
         </div>
 
-        <div className="w-11/12 md:w-5/12 mx-auto">
+        <div className="w-11/12 md:w-6/12 mx-auto pl-2">
           <div className="lg:pr-10">
             <div className="p-3 text-center inline-flex justify-center w-12 h-12 mb-2 md:w-16 md:h-16 md:mb-6 shadow-lg rounded-full bg-white">
               <p className="text-2xl md:text-3xl">🌎</p>
@@ -22,6 +22,7 @@ const AboutUsSection = (props: any) =>
               <img src="./assets/images/lifepack.webp" alt="Logo aliado Lifepack" className="h-16 mr-2 md:h-24 md:mr-2" />
               <img src="./assets/images/kaoba.webp" alt="Logo aliado Kaoba" className="h-16 mr-2 md:h-24 md:mr-2" />
               <img src="./assets/images/grafincol.webp" alt="Logo aliado Grafincol" className="h-12 mr-2 md:h-14" />
+              <img src="./assets/images/induecon.webp" alt="Logo aliado Induecon" className="h-12" />
             </div>
           </div>
         </div>
